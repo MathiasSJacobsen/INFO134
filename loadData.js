0e574536a478eab0de9e740b83fe5ddf12608352
kareneiane <kareneiane@hotmail.com>
2020-03-16 13:45:56 +0100

    small changes

--- loadData.js
+++ loadData.js
@@ -3,29 +3,82 @@ let sysselsatteURL = new URL('http://wildboy.uib.no/~tpe056/folk/100145.json');
 let utdanningURL = new URL('http://wildboy.uib.no/~tpe056/folk/85432.json');
 
 let xhr = new XMLHttpRequest();
+let responseObj;
 
-xhr.open("GET", befolkningURL);
-xhr.send();
-xhr.onload = function() {
-    let responseObj = xhr.response;
-    console.log(responseObj);
+//xhr.open("GET", befolkningURL);
+//xhr.responseType = 'json';
+//xhr.send();
+//xhr.onload = function() {
+  //  responseObj = xhr.response;
+    //console.log(responseObj.elementer);
+    //let befolkning = xhr.response;
+    //console.log(JSON.parse(responseObj)["elementer"]);
+//};
+
+function CBefolkning(URL) {
+    xhr.open("GET", URL);
+    xhr.responseType = "json";
+    xhr.send();
+    let responseBefolkning;
+    xhr.onload = function() {
+        this.responseBefolkning = xhr.response;
+        //console.log(responseBefolkning);
+        
+    }
+      getnames = function() {
+        responseBefolkning.elementer.forEach(element => {
+            console.log(element);
+            
+        });
+        responseBefolkning.elementer
+    }
 };
 
-xhr.open("GET", sysselsatteURL);
-xhr.send();
-xhr.onload = function() {
-    let responseObj = xhr.response;
-    console.log(responseObj);
-};
-
-xhr.open("GET", utdanningURL);
-xhr.send();
-xhr.onload = function() {
-    let responseObj = xhr.response;
-    console.log(responseObj);
-};
+let befolkningJSON = CBefolkning(befolkningURL);
+befolkningJSON.getames;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// xhr.open("GET", sysselsatteURL);
+// xhr.send();
+// xhr.onload = function() {
+//     let responseObj = xhr.response;
+    
+// };
+
+// xhr.open("GET", utdanningURL);
+// xhr.send();
+// xhr.onload = function() {
+//     let responseObj = xhr.response;
+    
+// };
+
+
+function befolkning() {
+    console.log(this.responseObj);
+}
+
+befolkning();
 
 
 
