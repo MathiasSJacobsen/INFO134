57e195564092defff2d46967d2caf596a9178aa3
Magnus <magnus.tonnessen97@gmail.com>
2020-03-17 17:24:31 +0100

    Get details about kommune from kommunenummer

--- src/js/Education.js
+++ src/js/Education.js
@@ -8,6 +8,29 @@ class Education {
         this.loadData();
     }
 
+    getHigherEducationPercent(municipalityName) {
+        let bachelorMen = this.data.elementer[municipalityName]["03a"]["Menn"]["2017"];
+        let bachelorWomen = this.data.elementer[municipalityName]["03a"]["Kvinner"]["2017"];
+        let masterMen = this.data.elementer[municipalityName]["04a"]["Menn"]["2017"];
+        let masterWomen = this.data.elementer[municipalityName]["04a"]["Kvinner"]["2017"];
+        let total = new Number(bachelorMen + bachelorWomen + masterMen + masterWomen);
+        return total.toFixed(2);
+    }
+
+    getHigherEducationQuantity(municipalityName) {
+        let percent = this.getHigherEducationPercent(municipalityName);
+        let totalPopulation = population.getTotalPopulation(municipalityName);
+        return Number(totalPopulation * percent / 100).toFixed(0);
+    }
+
+    getLastYearMeasured(municipalityName) {
+        let year = 2020;
+        while (!this.data.elementer[municipalityName]["Menn"][year.toString()]) {
+            year--;
+        }
+        return year;
+    }
+
     loadData() {
         this.dataLoader.extractData(this.onload);
     }
--- src/js/Employment.js
+++ src/js/Employment.js
@@ -8,6 +8,14 @@ class Employment {
         this.loadData();
     }
 
+    getLastYearMeasured(municipalityName) {
+        let year = 2020;
+        while (!this.data.elementer[municipalityName]["Menn"][year.toString()]) {
+            year--;
+        }
+        return year;
+    }
+    
     loadData() {
         this.dataLoader.extractData(this.onload);
     }
