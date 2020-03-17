63a8c932130e4d59f4313379c299c46aed41b8fe
Magnus <magnus.tonnessen97@gmail.com>
2020-03-17 18:55:19 +0100

    Deleted stupid while loop

--- src/js/Education.js
+++ src/js/Education.js
@@ -8,11 +8,11 @@ class Education {
         this.loadData();
     }
 
-    getHigherEducationPercent(municipalityName) {
-        let bachelorMen = this.data.elementer[municipalityName]["03a"]["Menn"][this.getLastYearMeasured(municipalityName)];
-        let bachelorWomen = this.data.elementer[municipalityName]["03a"]["Kvinner"][this.getLastYearMeasured(municipalityName)];
-        let masterMen = this.data.elementer[municipalityName]["04a"]["Menn"][this.getLastYearMeasured(municipalityName)];
-        let masterWomen = this.data.elementer[municipalityName]["04a"]["Kvinner"][this.getLastYearMeasured(municipalityName)];
+    getHigherEducationPercent(municipalityName, year=2017) {
+        let bachelorMen = this.data.elementer[municipalityName]["03a"]["Menn"][year];
+        let bachelorWomen = this.data.elementer[municipalityName]["03a"]["Kvinner"][year];
+        let masterMen = this.data.elementer[municipalityName]["04a"]["Menn"][year];
+        let masterWomen = this.data.elementer[municipalityName]["04a"]["Kvinner"][year];
         let total = new Number(bachelorMen + bachelorWomen + masterMen + masterWomen);
         return total.toFixed(2);
     }
@@ -21,14 +21,6 @@ class Education {
         let percent = this.getHigherEducationPercent(municipalityName);
         let totalPopulation = population.getTotalPopulation(municipalityName);
         return Number(totalPopulation * percent / 100).toFixed(0);
-    }
-
-    getLastYearMeasured(municipalityName) {
-        let year = 2020;
-        while (!this.data.elementer[municipalityName]["Menn"][year.toString()]) {
-            year--;
-        }
-        return year;
     }
 
     loadData() {
