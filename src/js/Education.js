8d4d361f537fb3460a4a9b8cdaf72c1af1ac93ce
Magnus <magnus.tonnessen97@gmail.com>
2020-03-18 15:49:47 +0100

    Fix education class

--- src/js/Education.js
+++ src/js/Education.js
@@ -37,12 +37,17 @@ class Education {
         }
     }
 
+    getEducationPercent(municipalityName, eduCat, gender, year=2017) {
+        let education = this.data.elementer[municipalityName.toString()][eduCat.toString()][gender.toString()][year.toString()];
+        return new Number(education).toFixed(2);
+    }
+
     getHigherEducationPercent(municipalityName, year=2017) {
-        let masterMen = this.data.elementer[municipalityName.toString()]["04a"]["Menn"][year.toString()];
-        let masterWomen = this.data.elementer[municipalityName.toString()]["04a"]["Kvinner"][year.toString()];
-        let bachelorMen = this.data.elementer[municipalityName.toString()]["03a"]["Menn"][year.toString()] - masterMen;
-        let bachelorWomen = this.data.elementer[municipalityName.toString()]["03a"]["Kvinner"][year.toString()] - masterWomen;
-        let total = bachelorMen + bachelorWomen + masterMen + masterWomen;
+        let masterMen = new Number(this.getEducationPercent(municipalityName, "04a", "Menn", year));
+        let masterWomen = new Number(this.getEducationPercent(municipalityName, "04a", "Kvinner", year));
+        let bachelorMen = new Number(this.getEducationPercent(municipalityName, "03a", "Menn", year));
+        let bachelorWomen = new Number(this.getEducationPercent(municipalityName, "03a", "Kvinner", year));
+        let total = new Number((bachelorMen + bachelorWomen + masterMen + masterWomen)/4);
         return total.toFixed(2);
     }
 
